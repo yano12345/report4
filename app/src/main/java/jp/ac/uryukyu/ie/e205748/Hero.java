50a5828764c7155486e8e17a5cb76341fee9a7b1
Yano Kotaro <e205748@ie.u-ryukyu.ac.jp>
2021-02-20 02:58:42 +0900

    1st commit

---
+++ app/src/main/java/jp/ac/uryukyu/ie/e205748/Hero.java
@@ -0,0 +1,70 @@
+package jp.ac.uryukyu.ie.e205748;
+
+/**
+ * ヒーロークラス。
+ *  String name; //敵の名前
+ *  int hitPoint; //敵のHP
+ *  int attack; //敵の攻撃力
+ *  boolean dead; //敵の生死状態。true=死亡。
+ * Created by tnal on 2016/11/13.
+ */
+public class Hero {
+    private String name;
+    private int hitPoint;
+    private int attack;
+    private boolean dead;
+    
+    /**
+     * 他のクラスでも参照出来る様にする。
+     * @return　ヒーローの名前
+     */
+    public String getName(){
+        return this.name;
+    }
+    /**
+     * 他のクラスでも参照出来る様にする。
+     * @return ヒーローの生死
+     */
+    public boolean getDead(){
+        return this.dead;
+    }
+
+
+    /**
+     * コンストラクタ。名前、最大HP、攻撃力を指定する。
+     * @param name ヒーロー名
+     * @param maximumHP ヒーローのHP
+     * @param attack ヒーローの攻撃力
+     */
+    public Hero (String name, int maximumHP, int attack) {
+        this.name = name;
+        hitPoint = maximumHP;
+        this.attack = attack;
+        dead = false;
+        System.out.printf("%sのHPは%d。攻撃力は%dです。\n", name, maximumHP, attack);
+    }
+
+    /**
+     * Enemyへ攻撃するメソッド。
+     * attackに応じて乱数でダメージを算出し、hero.wounded()によりダメージ処理を実行。
+     * @param e 攻撃対象
+     */
+    public void attack(Enemy e){
+        int damage = (int)(Math.random() * attack);
+        System.out.printf("%sの攻撃！%sに%dのダメージを与えた！！\n", name, getName(), damage);
+        e.wounded(damage);
+    }
+
+    /**
+     * 自身へ攻撃されたときのダメージ処理をするメソッド。
+     * 指定されたダメージを hitPoint から引き、死亡判定を行う。
+     * @param damage 受けたダメージ
+     */
+    public void wounded(int damage){
+        hitPoint -= damage;
+        if( hitPoint < 0 ) {
+            dead = true;
+            System.out.printf("勇者%sは道半ばで力尽きてしまった。\n", name);
+        }
+    }
+}
